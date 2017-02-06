bc1c68635150e5e8f40a09209696e6ae3425e17d
Sigi <shaiyaaccount2@web.de>
2017-02-06 22:25:11 +0100

    Adds !stoptranslate command.

---
+++ src/main/java/commands/translation/StopTranslateCommand.java
@@ -0,0 +1,71 @@
+package commands.translation;
+
+import commands.base.BasicCommand;
+import commands.translation.TranslateCommand.TranslateListener;
+import exceptions.CommandExecutionException;
+import net.dv8tion.jda.core.entities.User;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import properties.PropertiesHandler;
+import properties.PropertiesManager;
+
+/**
+ * Stops translation of a user's messages.
+ *
+ * @author Sigi
+ *
+ */
+public class StopTranslateCommand extends BasicCommand {
+
+	@Override
+	public String getName() {
+		return "stoptranslate";
+	}
+
+	@Override
+	public String getShortDescription() {
+		return "Stops translation of a user's messages.";
+	}
+
+	@Override
+	public String getExampleUsage() {
+		return "!stoptranslate @BrotBerserker";
+	}
+
+	@Override
+	public String execute(MessageReceivedEvent event, String... parameters) {
+		User user = event.getMessage().getMentionedUsers().get(0);
+		try {
+			for (Object listener : event.getJDA().getRegisteredListeners()) {
+				if (listener instanceof TranslateListener) {
+					if (((TranslateListener) listener).user.equals(user)) {
+						event.getJDA().removeEventListener(listener);
+					}
+				}
+			}
+			PropertiesHandler translations = PropertiesManager.getTranslationsForGuild(event.getGuild());
+			boolean removed = translations.remove(user.getName() + "#" + user.getDiscriminator());
+			return removed ? "Stopped translation for " + user.getName() + "!" : "There was no translation to stop!";
+		} catch (Exception e) {
+			throw new CommandExecutionException(e);
+		}
+	}
+
+	@Override
+	public int getRequiredParameterCount() {
+		return 1;
+	}
+
+	@Override
+	public boolean isExecutable(MessageReceivedEvent event, String... parameters) {
+		if (event.getMessage().getMentionedUsers().size() != 1) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String getErrorMessage() {
+		return "Please mention a user as 1. parameter!";
+	}
+
+}
